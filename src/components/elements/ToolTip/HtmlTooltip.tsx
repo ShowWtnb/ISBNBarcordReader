4c9d09911dd2a4820aa1da50b5d371180bf0febc
ShowWtnb <sho9021watanabe@gmail.com>
2023-07-29 12:02:44 +0900

    Add smartphone tooltip

--- src/components/elements/ToolTip/HtmlTooltip.tsx
+++ src/components/elements/ToolTip/HtmlTooltip.tsx
@@ -1,10 +1,20 @@
-import * as React from 'react';
+import * as React  from 'react';
 import { styled } from '@mui/material/styles';
 import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
 
-export const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
-  <Tooltip {...props} classes={{ popper: className }} />
-))(({ theme }) => ({
+export const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => {
+  const [leaveDelay, setLeaveDelay] = React.useState<number>(200)
+  function onTooltipTouchStart(event: React.TouchEvent<HTMLDivElement>): void {
+    setLeaveDelay(5000)
+  }
+  function onTooltipClose(event: Event | React.SyntheticEvent<Element, Event>): void {
+    setLeaveDelay(200)
+  }
+  return (
+    <Tooltip {...props} classes={{ popper: className }} leaveDelay={leaveDelay} onTouchStart={onTooltipTouchStart} onClose={onTooltipClose} />
+    // <Tooltip {...props} classes={{ popper: className }} />
+  );
+})(({ theme }) => ({
   [`& .${tooltipClasses.tooltip}`]: {
     backgroundColor: '#f5f5f9',
     color: 'rgba(0, 0, 0, 0.87)',
@@ -13,3 +23,28 @@ export const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
     border: '1px solid #dadde9',
   },
 }));
+
+// export default function HtmlTooltip() {
+//   function onTooltipTouchStart(event: React.TouchEvent<HTMLDivElement>): void {
+//     throw new Error('Function not implemented.');
+//   }
+//   function onTooltipClose(event: Event | React.SyntheticEvent<Element, Event>): void {
+//     throw new Error('Function not implemented.');
+//   }
+//   return (
+//     <>
+//       {styled(({ className, ...props }: TooltipProps) => (
+//         <Tooltip {...props} classes={{ popper: className }} onTouchStart={onTooltipTouchStart} onClose={onTooltipClose} />
+//       ))(({ theme }) => ({
+//         [`& .${tooltipClasses.tooltip}`]: {
+//           backgroundColor: '#f5f5f9',
+//           color: 'rgba(0, 0, 0, 0.87)',
+//           maxWidth: 220,
+//           fontSize: theme.typography.pxToRem(12),
+//           border: '1px solid #dadde9',
+//         },
+//       }))}
+//     </>
+
+//   );
+// }
--- src/components/elements/barcord_reader/BarcodeReader.tsx
+++ src/components/elements/barcord_reader/BarcodeReader.tsx
@@ -5,21 +5,22 @@ import { Box, Button, Checkbox, CircularProgress, FormControl, Grid, IconButton,
 import React, { useState, useEffect } from 'react'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
-import { HtmlTooltip } from "../ToolTip/HtmlTooltip";
+// import HtmlTooltip from "../ToolTip/HtmlTooltip";
+import {HtmlTooltip} from "../ToolTip/HtmlTooltip";
 import { GetLocalStorage, SaveLocalStorage } from "@/utils/LocalStorage";
 import { NextApiResponse } from "next";
 
 const LOCAL_ID = 'ISBN_BARCODE_READER';
 const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: '#0000',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: '#0000',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
 };
 
 
